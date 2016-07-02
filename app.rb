02727778f63759eed5e047034ed3ae0b83cc13d1
tktk <tktk.stereoman@gmail.com>
2016-07-02 20:18:43 +0900

    add /linebot/message GET API

--- app.rb
+++ app.rb
@@ -5,6 +5,20 @@ require 'json'
 require 'rest-client'
 
 class App < Sinatra::Base
+
+  def line_post(content_json)
+    endpoint_uri = 'https://trialbot-api.line.me/v1/events'
+    RestClient.proxy = ENV['FIXIE_URL'] if ENV['FIXIE_URL']
+    RestClient.post(endpoint_uri, content_json, {
+      'Content-Type' => 'application/json; charset=UTF-8',
+      'X-Line-ChannelID' => ENV["LINE_CHANNEL_ID"],
+      'X-Line-ChannelSecret' => ENV["LINE_CHANNEL_SECRET"],
+      'X-Line-Trusted-User-With-ACL' => ENV["LINE_CHANNEL_MID"],
+    })
+  end
+
+  to_id = ENV['LINE_TO_ID']
+
   post '/linebot/callback' do
     params = JSON.parse(request.body.read)
 
@@ -15,19 +29,30 @@ class App < Sinatra::Base
         eventType: "138311608800106203", # Fixed value
         content: msg['content']
       }
-
-      endpoint_uri = 'https://trialbot-api.line.me/v1/events'
       content_json = request_content.to_json
-
-      RestClient.proxy = ENV['FIXIE_URL'] if ENV['FIXIE_URL']
-      RestClient.post(endpoint_uri, content_json, {
-        'Content-Type' => 'application/json; charset=UTF-8',
-        'X-Line-ChannelID' => ENV["LINE_CHANNEL_ID"],
-        'X-Line-ChannelSecret' => ENV["LINE_CHANNEL_SECRET"],
-        'X-Line-Trusted-User-With-ACL' => ENV["LINE_CHANNEL_MID"],
-      })
+      line_post(content_json)
     end
 
     "OK"
   end
+
+  get '/linebot/message/:text' do
+    content = {
+      contentType: 1,
+      toType: 1,
+      "text": params[:text]
+    }
+
+    request_content = {
+      to: [to_id],
+      toChannel: 1383378250, # Fixed  value
+      eventType: "138311608800106203", # Fixed value
+      content: content
+    }
+
+    content_json = request_content.to_json
+    line_post(content_json)
+    "OK"
+  end
+
 end
